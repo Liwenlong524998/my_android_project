be27c9812a15705f93524c6bd8587a44199dc21b
Liwenlong524998 <2080524998@qq.com>
2020-12-06 11:08:23 +0800

    对通知接收的修改

--- app/src/main/java/com/example/myapplication/parent.java
+++ app/src/main/java/com/example/myapplication/parent.java
@@ -22,11 +22,13 @@ public class parent extends AppCompatActivity {
     private Button button;
     private TextView txt;
     private dataBase database;
+    private SQLiteDatabase db;
     private String ID;
     private Bundle receive;
-    private Intent for_send;
-    private Bundle send;
+    private Intent send;
+    private Bundle for_send;
     private TextView check;
+    private String student_number;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,18 +40,20 @@ public class parent extends AppCompatActivity {
 
         linearLayout=(LinearLayout)findViewById(R.id.linearLayout1);
         scrollView=(ScrollView)findViewById(R.id.scrollView);
+
         database=new dataBase(this,"leave",null,4);
-        SQLiteDatabase db=database.getWritableDatabase();
+        db=database.getWritableDatabase();
+
         String select_id="select * from Parents where account='"+ID+"'";
         Log.i("Ex04","selectDB"+select_id);
         System.out.println("+++++++++++++++++++++++++++");
         Log.i("Ex04","selectDB"+select_id);
         final Cursor cursor1=db.rawQuery(select_id,null);
         StringBuilder s_id= new StringBuilder();
-        String student_number="";
+        student_number="";
         String isagree="";
-        for_send=new Intent();
-        send=new Bundle();
+        send=new Intent();
+        for_send=new Bundle();
         int i=0;
 
         while (cursor1.moveToNext())
@@ -94,13 +98,13 @@ public class parent extends AppCompatActivity {
             button.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    send.putString("student_number", finalStudent_number.toString());
-                    send.putString("parentORteacher","parent");
+                    for_send.putString("student_number", finalStudent_number.toString());
+                    for_send.putString("parentORteacher","parent");
                     System.out.println("++++++++++++++++++++++++");
                     System.out.println(finalStudent_number);
-                    for_send.putExtras(send);
-                    for_send.setClass(parent.this,for_leaveMessage.class);
-                    startActivity(for_send);
+                    send.putExtras(for_send);
+                    send.setClass(parent.this,for_leaveMessage.class);
+                    startActivity(send);
                 }
             });
             linearLayout1.addView(button);
@@ -110,13 +114,54 @@ public class parent extends AppCompatActivity {
         }
         System.out.println(s_id);
         System.out.println(i);
-        /*if (i!=0)
-        {
-            while (cursor1.moveToNext())
-            {
 
-            }
-        }*/
+
+        String select_class="select * from students where stu_number='"+student_number+"'";
+        String select_course="select * from StudentTeacher where stu_number='"+student_number+"'";
+        Log.i("Ex04","selectDB="+select_class);
+        Cursor cursor2=db.rawQuery(select_class,null);
+        String classID="";
+        String course_number="";
+        while (cursor2.moveToNext())
+        {
+            classID=cursor2.getString(cursor2.getColumnIndex("class_room"));
+        }
+        cursor2.close();
+        Log.i("Ex04","selectDB="+select_course);
+        Cursor cursor3=db.rawQuery(select_course,null);
+        while (cursor3.moveToNext())
+        {
+            course_number=cursor3.getString(cursor3.getColumnIndex("course_number"));
+        }
+        cursor3.close();
+        String select_notice="select * from notice where course_number='"+course_number+"' or class_name='"+classID+"'";
+        Log.i("Ex04","selectDB="+select_notice);
+        final Cursor cursor4 =db.rawQuery(select_notice,null);
+        String notice_context="";
+        while(cursor4.moveToNext())
+        {
+            notice_context=cursor4.getString(cursor4.getColumnIndex("notice_context"));
+            linearLayout1=new LinearLayout(this);
+            txt=new TextView(this);
+            txt.setText("您有一条新的通知！");
+            button=new Button(this);
+            button.setText("查看");
+            linearLayout1.addView(button);
+            linearLayout1.addView(txt);
+            linearLayout.addView(linearLayout1);
+            final String finalNotice_context = notice_context;
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    for_send.putString("type","notice");
+                    for_send.putString("context", finalNotice_context);
+                    send.putExtras(for_send);
+                    send.setClass(parent.this,detail_message.class);
+                    startActivity(send);
+                }
+            });
+        }
+        cursor4.close();
 
 
 
